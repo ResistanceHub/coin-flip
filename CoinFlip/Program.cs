a28cdea14e0e294b5737e93f72b2ceb3b67beb4f
Dilini Prasangika <dilini.prasangika@insightsoftware.com>
2018-05-03 10:34:16 +0100

    Changed the program.cs to print how many times it landed on heads and how many times it landed on tails

--- CoinFlip/Program.cs
+++ CoinFlip/Program.cs
@@ -10,14 +10,33 @@ namespace CoinFlip
 	{
 		static void Main(string[] args)
 		{
-			Random random = new Random(); 
+			Console.WriteLine("Flipping a coin 1000 times");
+			CoinFlip();
+		}
 
-			for (int i = 0; i < 5; i++)
+
+		private static void CoinFlip()
+		{
+            Random random = new Random(); 
+			int noOfHeads = 0;
+			int noOfTails = 0;
+
+			for (int i = 0; i < 1000; i++)
 			{
-				int rnd = random.Next(0, 5); // https://msdn.microsoft.com/en-us/library/2dx6wyd4 - the upper bound is scluded, this will randomly produce one of these values: 0, 1, 2, 3, 4 - it will not produce 5
-				Console.WriteLine(i.ToString() + ' ' + rnd);
+				int rnd = random.Next(0, 2); // https://msdn.microsoft.com/en-us/library/2dx6wyd4 - the upper bound is scluded, this will randomly produce one of these values: 0, 1, 2, 3, 4 - it will not produce 5
+				
+				if(rnd == 1)
+				{
+					noOfHeads++;
+				}
+				else
+				{
+					noOfTails++;
+				}				
 			}
-
+			//Console.WriteLine(i.ToString() + ' ' + rnd);
+			Console.WriteLine("Heads ware flipped {0} times", noOfHeads);
+            Console.WriteLine("Tails was flipped {0} times", noOfTails);
 			Console.ReadKey();
 		}
 	}

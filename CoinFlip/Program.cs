3a7af84b48b97d9d5e05c709523035cc6d96d911
Ping Wu <Ping.Wu@insightsoftware.com>
2018-05-03 13:54:32 +0100

    Sum up the Head and Tail

--- CoinFlip/Program.cs
+++ CoinFlip/Program.cs
@@ -11,15 +11,25 @@ namespace CoinFlip
 		static void Main(string[] args)
 		{
 			Random random = new Random();
+		    int head = 0;
+		    int tail = 0;
 
-		    Console.WriteLine("\n flip coin 0 or 1:"); 
-            for (int i = 1; i <=100; i++)
-			{
-				int rnd = random.Next(0, 2); // https://msdn.microsoft.com/en-us/library/2dx6wyd4 - the upper bound is scluded, this will randomly produce one of these values: 0, 1, 2, 3, 4 - it will not produce 5
-				Console.WriteLine(i.ToString() + ' ' + rnd);
-			}
-
-			Console.ReadKey();
+            Console.WriteLine("Flip 1 coin 1000 times with Head (0) or Tails (1):"); 
+            for (int i = 1; i <=1000; i++)
+            {
+				int rnd = random.Next(0, 2); // https://msdn.microsoft.com/en-us/library/2dx6wyd4 - the upper bound is scluded, this will randomly produce one of these values: 0, 1
+				//Console.WriteLine(i.ToString() + ' ' + rnd);               
+                if (rnd ==0)
+                {
+                    head = head + 1;
+                }
+                else tail = tail + 1;
+                
+            }
+          
+            Console.WriteLine("Total Heads" + ' ' + head);
+		    Console.WriteLine("Total Tails" + ' ' + tail);
+            Console.ReadKey();
 		}
 	}
 }

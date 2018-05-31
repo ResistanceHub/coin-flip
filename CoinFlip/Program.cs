29d22e7e5ef227a5ead794e9f5eed2e252aef843
Ping Wu <Ping.Wu@insightsoftware.com>
2018-05-31 17:05:22 +0100

    change enum's name to CoinSides, heads to headsCount, tails to tailsCount, removed unwanted comment

--- CoinFlip/Program.cs
+++ CoinFlip/Program.cs
@@ -1,58 +1,54 @@
 ﻿using System;
 using System.IO;
-using static CoinFlip.CoinFlip;
+using System.Runtime.Remoting.Messaging;
 
 namespace CoinFlip
 {
-    public enum CoinFlip
+    public enum CoinSides
     {
         Heads = 0,
         Tails = 1
-
     }
-    class Program
+
+    public class Program
     {
         static void Main(string[] args)
         {
             var random = new Random();
-            var heads = 0;
-            var tails = 0;
+            var headsCount = 0;
+            var tailsCount = 0;
             var path = @"C:\dev\coin-flip\CoinFlip\ping_flipping\coin_result.csv";
           
-
-            //console print title
             Console.WriteLine("Flip 1 coin 1000 times with Heads (0) or Tails (1):");
 
             //file print results          
             using (var flippingresults = new StreamWriter(path))
             {
-                //file print title
                 flippingresults.WriteLine("Flip 1 coin 1000 times with Head (0) or Tails (1):");
 
+                var Heads = default(CoinSides);
                 for (var i = 1; i <= 1000; i++)
                 {
                     var rnd = random.Next(0, 2); // https://msdn.microsoft.com/en-us/library/2dx6wyd4 - the upper bound is scluded, this will randomly produce one of these values: 0, 1                                                              
-                    var coinValue = (CoinFlip) rnd;
-                   
-                    //console print detailed flipping result   
+                    var coinValue = (CoinSides) rnd;
+
                     Console.WriteLine(i.ToString() + "," + coinValue.ToString());
                    
                     //file print detailed flipping result
                     flippingresults.WriteLine(i.ToString() + "," + coinValue.ToString());
 
                     if (Heads == coinValue)
-                        heads = heads + 1;
+                        headsCount = headsCount + 1;
                     else
-                        tails = tails + 1;
+                        tailsCount = tailsCount + 1;
                 }
 
-                //console print total
-                Console.WriteLine("Total Heads" + " " + heads);
-                Console.WriteLine("Total Tails" + " " + tails);
+                Console.WriteLine("Total Heads" + " " + headsCount);
+                Console.WriteLine("Total Tails" + " " + tailsCount);
 
                 //file print total
-                flippingresults.WriteLine("Total Heads" + " " + heads);
-                flippingresults.WriteLine("Total Tails" + " " + tails);
+                flippingresults.WriteLine("Total Heads" + " " + headsCount);
+                flippingresults.WriteLine("Total Tails" + " " + tailsCount);
             }
 
             Console.ReadKey();

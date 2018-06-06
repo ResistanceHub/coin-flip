8c0ce6c04c47b936d2f36799f3015c2025ff18f8
Ping Wu <Ping.Wu@insightsoftware.com>
2018-06-06 10:02:29 +0100

    updated program with function - return sides from List

--- CoinFlip/Program.cs
+++ CoinFlip/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 
 
@@ -8,41 +9,55 @@ namespace CoinFlip
    
     public class Program
     {
-        public enum CoinFlip
+        static Random random = new Random();// put it out side of method to make one instance
+        public enum Sides
         {
-            Heads = 0,
-            Tails = 1
+            Heads,
+            Tails
         }
-        static int FlipValue()
+        //This returns one coin side (1 value)
+        static Sides Flip() // use enum type 
         {
-            var random = new Random();
-            var rnd = random.Next(0, 2); https://msdn.microsoft.com/en-us/library/2dx6wyd4
-            return rnd;   //how to return enum type item? What is the type of 'Heads'?
+            var randomNumber = random.Next(0, 2); //https://msdn.microsoft.com/en-us/library/2dx6wyd4
+            var randomCoinSide = (Sides) randomNumber; //cast the randomNumber to enum type (ie. randomCoinSide)
+            return randomCoinSide;
         }
+
+        // This method return 1000 random coin sides by add them into a list
+            static List<Sides> FlipCoins()
+            {
+              List<Sides> coinSides = new List<Sides>();
+                for (var i = 1; i <= 1000; i++)
+                	{
+                	    var randomSide = Flip();
+                		coinSides.Add(randomSide);
+                	}
+            		return coinSides;
+            }
+    
         static void Main(string[] args)
         {
             var headsCount = 0;
             var tailsCount = 0;
-            var path = @"C:\dev\coin-flip\CoinFlip\ping_flipping\coin_result.csv";
+            var path = @"C:\dev\coin-flip\CoinFlip\ping_flipping\coin_result.csv";//
           
-            Console.WriteLine("Flip 1 coin 1000 times with Heads (0) or Tails (1):");
+            Console.WriteLine("Flip 1 coin 1000 times with Heads or Tails:");
 
             //file print results          
-            var heads = default(CoinFlip);
             using (var flippingresults = new StreamWriter(path))
             {
-                flippingresults.WriteLine("Flip 1 coin 1000 times with Head (0) or Tails (1):");
+                flippingresults.WriteLine("Flip 1 coin 1000 times with Head or Tails:");
 
+                var coinSides = FlipCoins();
                 for (var i = 1; i <= 1000; i++)
                 {
-                  var coinSide = (CoinFlip) FlipValue();
+                Console.WriteLine($"{i} , {coinSides[i-1]}");//string interpolation
 
-                    Console.WriteLine($"{i} , {coinSide}");
 
-                    //file print detailed flipping result
-                    flippingresults.WriteLine($"{i} , {coinSide}");//string interpolation
-                  
-                    if (heads == coinSide)
+                    //file print detailed flipping result from list
+                    flippingresults.WriteLine($"{i} ,  {coinSides[i-1]}");
+
+                    if (Sides.Heads == coinSides[i-1])
                         headsCount = headsCount + 1;
                     else
                         tailsCount = tailsCount + 1;

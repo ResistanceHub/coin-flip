ec0ae6e223afd17f8ccef2f43235b9ecc9f0827d
Shikha Jakhar <shikha.jakhar@insightsoftware.com>
2018-06-29 12:08:44 +0100

    writing to the file

--- CoinFlip/Program.cs
+++ CoinFlip/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,15 +12,19 @@ namespace FlippingCoins
         static void Main(string[] args)
         {
             var Coins = 0;
+            int NoOfHeads = 0;
+            int NoOfTails = 0;
+
             Console.WriteLine("Enter number of coins to be flipped");
             Coins = Convert.ToInt32(Console.ReadLine());
-            FlippingCoinFunction(Coins);
-
+            NoOfHeads = FlippingCoinFunction(Coins);
+            NoOfTails = Coins-NoOfHeads;
+            WriteToFileFunction(NoOfHeads, NoOfTails);
 
 
         }
 
-        static void FlippingCoinFunction(int Coin)
+        static int FlippingCoinFunction(int Coin)
         {
             int Heads = 0;
             int Tails = 0;
@@ -36,8 +41,18 @@ namespace FlippingCoins
             Console.WriteLine("No of Heads =" + Heads);
             Console.WriteLine("No of Tails =" + Tails);
             Console.ReadKey();
+            return (Heads);
+        }
 
+        static void WriteToFileFunction(int Heads, int Tails)
+        {
+            using (StreamWriter writetext = new StreamWriter("MyFile.txt"))
+            {
+                writetext.WriteLine("Number of Heads = "+Heads +Environment.NewLine);
+                writetext.WriteLine("Number of Tails = " +Tails + Environment.NewLine);
+            }
         }
 
     }
 
+}
